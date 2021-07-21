9f0bc5a9259cd7ef5005346d361e1dcd4ef259bb
Sam Sinnamon <samsinnamon@hotmail.com>
2021-07-21 22:13:51 +1000

    remove unneccessary asyncio install requires

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="airtouch4pyapi", # Replace with your own username
-    version="1.0.1",
+    version="1.0.2",
     author="Sam Sinnamon",
     author_email="samsinnamon@hotmail.com",
     description="An api allowing control of AC state (temperature, on/off, mode) of an Airtouch 4 controller locally over TCP",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/LonePurpleWolf/airtouch4pyapi",
     packages=setuptools.find_packages(),
-    install_requires=['asyncio'],
+    install_requires=[''],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
